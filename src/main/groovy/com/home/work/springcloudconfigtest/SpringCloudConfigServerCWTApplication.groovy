116d6a62ef32bec91ac4fb1edce069015483f2ef
PuneethKumarCk <punith.564@gmail.com>
2017-12-22 21:20:03 +0530

    groovy based config server for cwt app

---
+++ src/main/groovy/com/home/work/springcloudconfigtest/SpringCloudConfigServerCWTApplication.groovy
@@ -0,0 +1,14 @@
+package com.home.work.springcloudconfigtest
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+
+@SpringBootApplication
+@EnableConfigServer
+class SpringCloudConfigServerCWTApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerCWTApplication.class, args)
+    }
+}
